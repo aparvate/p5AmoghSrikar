662f16c34674ab1b322306f57e56921f2df71597
aparvate <amogh.parvate@gmail.com>
2024-11-19 18:44:04 -0600

    Re-add map length

--- xv6-public/trap.c
+++ xv6-public/trap.c
@@ -81,20 +81,20 @@ trap(struct trapframe *tf)
     uint address_of_fault = rcr2();
     struct proc *p = myproc();
     struct map_wmap *map;
-    int page_fault_resolved = 0;
+    int segFaultFound = 0;
 
     // if page fault addr is part of a mapping: // lazy allocation
     // handle it
     for(int i=0; i<16; i++) {
       map = &p->maps[i];
       // we've reached the last filled element, so we can break since we know all elements after will also be invalid
-      // if(map->length > 0) {
-      //   break;
-      // }
+      if(map->length > 0) {
+        break;
+      }
 
       // check if address of fault is within the bounds
       if(address_of_fault >= map -> addr && address_of_fault < (map -> addr + map -> length)) {
-        page_fault_resolved = 1;
+        segFaultFound = 1;
 
         // allocate the page now
         char *mem = kalloc();
@@ -112,7 +112,7 @@ trap(struct trapframe *tf)
 
     }
       
-    if(page_fault_resolved == 0) {
+    if(segFaultFound == 0) {
       cprintf("Segmentation Fault\n");
       // kill the process
       p->killed = 1;
