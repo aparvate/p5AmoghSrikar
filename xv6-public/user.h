6c961fcc196de63c2d52a09ecf03e47c696d6ba1
Srikar-Bhuvan <124732014+Srikar-Bhuvan@users.noreply.github.com>
2024-11-19 18:24:10 -0600

    add va2pa user.h

--- xv6-public/user.h
+++ xv6-public/user.h
@@ -25,6 +25,7 @@ int sleep(int);
 int uptime(void);
 uint wmap(uint addr, int length, int flags, int fd);
 int wunmap(uint addr);
+int va2pa(uint va);
 struct wmapinfo;
 int getwmapinfo(struct wmapinfo*);
 
